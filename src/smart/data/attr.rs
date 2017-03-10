500062c74d89d0c109d4d0a0f6e5d76dda15aae6
vthriller <unixway.drive+git@gmail.com>
2017-03-11 01:48:43 +0300

    more flags based on smartmontools source code as well as a bunch of random datasheets

--- src/smart/data/attr.rs
+++ src/smart/data/attr.rs
@@ -1,9 +1,17 @@
 #[derive(Debug)]
 pub struct SmartAttribute<'a> {
 	id: u8,
+
 	pre_fail: bool, // if true, failure is predicted within 24h; otherwise, attribute indicates drive's exceeded intended design life period
 	online: bool,
+	// In SFF-8035i rev 2, bits 2-5 are defined as vendor-specific, and 6-15 are reserved;
+	// however, these days the following seems to be universally interpreted the way it was once (probably) established by IBM, Maxtor and Quantum
+	performance: bool,
+	error_rate: bool,
+	event_count: bool,
+	self_preserving: bool,
 	flags: u16,
+
 	value: u8, // TODO? 0x00 | 0xfe | 0xff are invalid
 	// vendor-specific:
 	worst: u8,
@@ -16,11 +24,20 @@ pub fn parse_smart_values<'a>(data: &'a [u8; 512]) -> Vec<SmartAttribute<'a>> {
 	for i in 0..30 {
 		let offset = 2 + i * 12;
 		if data[offset] == 0 { continue } // attribute table entry of id 0x0 is invalid
+
+		let flags = (data[offset + 1] as u16) + ((data[offset + 2] as u16) << 8); // XXX endianness?
+
 		attrs.push(SmartAttribute {
+
 			id: data[offset],
-			pre_fail: data[offset + 1] & (1<<0) != 0,
-			online: data[offset + 1] & (1<<1) != 0,
-			flags: ((data[offset + 1] & !(0b11)) as u16) + ((data[offset + 2] as u16) << 8), // XXX endianness?
+			pre_fail:        flags & (1<<0) != 0,
+			online:          flags & (1<<1) != 0,
+			performance:     flags & (1<<2) != 0,
+			error_rate:      flags & (1<<3) != 0,
+			event_count:     flags & (1<<4) != 0,
+			self_preserving: flags & (1<<5) != 0,
+			flags:           flags & (!0b111111),
+
 			value: data[offset + 3],
 			worst: data[offset + 4],
 			raw: &data[offset + 5 .. offset + 12],
