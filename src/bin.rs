1e9e30b058e7664c07e7043384a6bb4342bfd865
vthriller <unixway.drive+git@gmail.com>
2017-03-12 09:55:08 +0300

    move print!()s into separate functions (prepare for different output formats)

--- src/bin.rs
+++ src/bin.rs
@@ -20,6 +20,75 @@ fn bool_to_flag(b: bool, c: char) -> char {
 	if b { c } else { '-' }
 }
 
+fn print_id(id: &id::Id) {
+	if id.incomplete { print!("WARNING: device reports information it provides is incomplete\n\n"); }
+
+	// XXX id.is_ata is deemed redundant and is skipped
+	// XXX we're skipping id.commands_supported for now as it is hardly of any interest to users
+
+	print!("Model:    {}\n", id.model);
+	print!("Firmware: {}\n", id.firmware);
+	print!("Serial:   {}\n", id.serial);
+	// TODO: id.wwn_supported is cool, but actual WWN ID is better
+
+	print!("\n");
+
+	print!("ATA version:\n{}\n", id.ata_version.unwrap_or("unknown"));
+
+	print!("\n");
+
+	// The following guide, when printed, is exactly 80 characters
+	// ... "..............................................................supported disabled\n"
+	print!("Host protected area:           {}\n", id.hpa);
+	print!("Advanced Power Management:     {}\n", id.apm);
+	print!("Automatic Acoustic Management: {}\n", id.aam);
+	print!("Read look-ahead:               {}\n", id.read_look_ahead);
+	print!("Write cache:                   {}\n", id.write_cache);
+	print!("Power management:              {}\n", bool_to_sup(id.power_mgmt_supported));
+	print!("General purpose logging:       {}\n", bool_to_sup(id.gp_logging_supported));
+	print!("Trusted computing:             {}\n", bool_to_sup(id.trusted_computing_supported));
+	print!("ATA security:                  {}\n", id.security);
+
+	print!("\n");
+
+	print!("S.M.A.R.T.:    {}\n", id.smart);
+	print!("Error logging: {}\n", bool_to_sup(id.smart_error_logging_supported));
+	print!("Self-test:     {}\n", bool_to_sup(id.smart_self_test_supported));
+
+	print!("\n");
+}
+
+fn print_attributes(values: &Vec<attr::SmartAttribute>) {
+	print!("S.M.A.R.T. attribute values:\n");
+	print!(" ID flags        value worst thresh raw\n");
+	for val in values {
+		print!("{:3} {}{}{}{}{}{} {:04x}    {:3}   {:3}    {} {:?}\n",
+			val.id,
+			bool_to_flag(val.pre_fail, 'P'),
+			bool_to_flag(!val.online, 'O'),
+			bool_to_flag(val.performance, 'S'),
+			bool_to_flag(val.error_rate, 'R'),
+			bool_to_flag(val.event_count, 'C'),
+			bool_to_flag(val.self_preserving, 'K'),
+			val.flags,
+			val.value, val.worst,
+			match val.thresh {
+				Some(t) => format!("{:3}", t),
+				None => "?".to_string(),
+			},
+			// TODO interpreted raw values
+			val.raw,
+		);
+	}
+	// based on the output of 'smartctl -A -f brief' (part of 'smartctl -x')
+	print!("    │││││└─ K auto-keep\n");
+	print!("    ││││└── C event count\n");
+	print!("    │││└─── R error rate\n");
+	print!("    ││└──── S speed/performance\n");
+	print!("    │└───── O updated during off-line testing\n");
+	print!("    └────── P prefailure warning\n");
+}
+
 fn main() {
 	let args = App::new("smart-rs")
 		.about("yet another S.M.A.R.T. querying tool")
@@ -57,41 +126,7 @@ fn main() {
 		let id = id::parse_id(&data);
 
 		if print_info {
-			if id.incomplete { print!("WARNING: device reports information it provides is incomplete\n\n"); }
-
-			// XXX id.is_ata is deemed redundant and is skipped
-			// XXX we're skipping id.commands_supported for now as it is hardly of any interest to users
-
-			print!("Model:    {}\n", id.model);
-			print!("Firmware: {}\n", id.firmware);
-			print!("Serial:   {}\n", id.serial);
-			// TODO: id.wwn_supported is cool, but actual WWN ID is better
-
-			print!("\n");
-
-			print!("ATA version:\n{}\n", id.ata_version.unwrap_or("unknown"));
-
-			print!("\n");
-
-			// The following guide, when printed, is exactly 80 characters
-			// ... "..............................................................supported disabled\n"
-			print!("Host protected area:           {}\n", id.hpa);
-			print!("Advanced Power Management:     {}\n", id.apm);
-			print!("Automatic Acoustic Management: {}\n", id.aam);
-			print!("Read look-ahead:               {}\n", id.read_look_ahead);
-			print!("Write cache:                   {}\n", id.write_cache);
-			print!("Power management:              {}\n", bool_to_sup(id.power_mgmt_supported));
-			print!("General purpose logging:       {}\n", bool_to_sup(id.gp_logging_supported));
-			print!("Trusted computing:             {}\n", bool_to_sup(id.trusted_computing_supported));
-			print!("ATA security:                  {}\n", id.security);
-
-			print!("\n");
-
-			print!("S.M.A.R.T.:    {}\n", id.smart);
-			print!("Error logging: {}\n", bool_to_sup(id.smart_error_logging_supported));
-			print!("Self-test:     {}\n", bool_to_sup(id.smart_self_test_supported));
-
-			print!("\n");
+			print_id(&id);
 		}
 
 		if print_attrs {
@@ -104,34 +139,7 @@ fn main() {
 				// TODO attribute names
 				// TODO when-failed (now/past/never)
 
-				print!("S.M.A.R.T. attribute values:\n");
-				print!(" ID flags        value worst thresh raw\n");
-				for val in values {
-					print!("{:3} {}{}{}{}{}{} {:04x}    {:3}   {:3}    {} {:?}\n",
-						val.id,
-						bool_to_flag(val.pre_fail, 'P'),
-						bool_to_flag(!val.online, 'O'),
-						bool_to_flag(val.performance, 'S'),
-						bool_to_flag(val.error_rate, 'R'),
-						bool_to_flag(val.event_count, 'C'),
-						bool_to_flag(val.self_preserving, 'K'),
-						val.flags,
-						val.value, val.worst,
-						match val.thresh {
-							Some(t) => format!("{:3}", t),
-							None => "?".to_string(),
-						},
-						// TODO interpreted raw values
-						val.raw,
-					);
-				}
-				// based on the output of 'smartctl -A -f brief' (part of 'smartctl -x')
-				print!("    │││││└─ K auto-keep\n");
-				print!("    ││││└── C event count\n");
-				print!("    │││└─── R error rate\n");
-				print!("    ││└──── S speed/performance\n");
-				print!("    │└───── O updated during off-line testing\n");
-				print!("    └────── P prefailure warning\n");
+				print_attributes(&values);
 			} else {
 				print!("S.M.A.R.T. is disabled, cannot show attributes\n")
 			}
