eeeeaa8034f43d58eebd4eb272b43b3853658e3b
vthriller <unixway.drive+git@gmail.com>
2017-03-29 04:01:56 +0300

    split drivedb into submodules

---
+++ src/smart/drivedb/mod.rs
@@ -0,0 +1,52 @@
+mod parser;
+pub use self::parser::Entry;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::io;
+
+use nom;
+
+use std::{error, fmt, convert};
+
+#[derive(Debug)]
+pub enum Error {
+	IO(io::Error),
+	Parse, // TODO? Parse(nom::verbose_errors::Err) if dependencies.nom.features = ["verbose-errors"]
+}
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			Error::IO(ref err) => write!(f, "IO error: {}", err),
+			Error::Parse => write!(f, "Parse error"),
+		}
+	}
+}
+impl error::Error for Error {
+	fn description(&self) -> &str {
+		match *self {
+			Error::IO(ref err) => err.description(),
+			Error::Parse => "malformed database",
+		}
+	}
+	fn cause(&self) -> Option<&error::Error> {
+		match *self {
+			Error::IO(ref err) => Some(err),
+			Error::Parse => None,
+		}
+	}
+}
+impl convert::From<io::Error> for Error {
+	fn from(err: io::Error) -> Error { Error::IO(err) }
+}
+
+pub fn load(file: &str) -> Result<Vec<Entry>, Error> {
+	let mut db = Vec::new();
+	File::open(&file)?.read_to_end(&mut db)?;
+
+	match parser::database(&db) {
+		nom::IResult::Done(_, entries) => Ok(entries),
+		nom::IResult::Error(_) => Err(Error::Parse),
+		nom::IResult::Incomplete(_) => unreachable!(), // XXX is it true?
+	}
+}
--- src/smart/drivedb/parser.rs
+++ src/smart/drivedb/parser.rs
@@ -1,11 +1,4 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io;
-
-use nom;
 use nom::multispace;
-
-use std::{error, fmt, convert};
 
 named!(comment_block, do_parse!(
 	tag!("/*") >>
@@ -100,7 +93,7 @@ named!(entry <Entry>, do_parse!(
 	})
 ));
 
-named!(database <Vec<Entry>>, do_parse!(
+named!(pub database <Vec<Entry>>, do_parse!(
 	whitespace >>
 	entries: many1!(do_parse!(
 		e: entry >> comma >> (e)
@@ -109,45 +102,3 @@ named!(database <Vec<Entry>>, do_parse!(
 	eof!() >>
 	(entries)
 ));
-
-#[derive(Debug)]
-pub enum Error {
-	IO(io::Error),
-	Parse, // TODO? Parse(nom::verbose_errors::Err) if dependencies.nom.features = ["verbose-errors"]
-}
-impl fmt::Display for Error {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match *self {
-			Error::IO(ref err) => write!(f, "IO error: {}", err),
-			Error::Parse => write!(f, "Parse error"),
-		}
-	}
-}
-impl error::Error for Error {
-	fn description(&self) -> &str {
-		match *self {
-			Error::IO(ref err) => err.description(),
-			Error::Parse => "malformed database",
-		}
-	}
-	fn cause(&self) -> Option<&error::Error> {
-		match *self {
-			Error::IO(ref err) => Some(err),
-			Error::Parse => None,
-		}
-	}
-}
-impl convert::From<io::Error> for Error {
-	fn from(err: io::Error) -> Error { Error::IO(err) }
-}
-
-pub fn load(file: &str) -> Result<Vec<Entry>, Error> {
-	let mut db = Vec::new();
-	File::open(&file)?.read_to_end(&mut db)?;
-
-	match database(&db) {
-		nom::IResult::Done(_, entries) => Ok(entries),
-		nom::IResult::Error(_) => Err(Error::Parse),
-		nom::IResult::Incomplete(_) => unreachable!(), // XXX is it true?
-	}
-}
