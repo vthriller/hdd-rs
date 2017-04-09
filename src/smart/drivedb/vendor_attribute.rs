4a6afd66127bd77d608b7448b96387e9a0b62202
vthriller <unixway.drive+git@gmail.com>
2017-04-10 02:32:20 +0300

    support for legacy attribute definitions

--- src/smart/drivedb/vendor_attribute.rs
+++ src/smart/drivedb/vendor_attribute.rs
@@ -49,6 +49,7 @@ named!(parse_standard <Attribute>, do_parse!(
 		take_till1_s!(not_comma_nor_colon),
 		str::from_utf8
 	) >>
+	// TODO '+' for ATTRFLAG_INCREASING
 	byte_order: opt!(complete!(do_parse!( // TODO len()<6 should be invalid
 		char!(':') >>
 		byteorder: map_res!(
@@ -97,6 +98,23 @@ named!(parse_standard <Attribute>, do_parse!(
 ));
 
 pub fn parse(s: &str) -> Result<Attribute, Error> {
+	let s = match s {
+		"9,halfminutes" => "9,halfmin2hour,Power_On_Half_Minutes",
+		"9,minutes" => "9,min2hour,Power_On_Minutes",
+		"9,seconds" => "9,sec2hour,Power_On_Seconds",
+		"9,temp" => "9,tempminmax,Temperature_Celsius",
+		"192,emergencyretractcyclect" => "192,raw48,Emerg_Retract_Cycle_Ct",
+		"193,loadunload" => "193,raw24/raw24",
+		"194,10xCelsius" => "194,temp10x,Temperature_Celsius_x10",
+		"194,unknown" => "194,raw48,Unknown_Attribute",
+		"197,increasing" => "197,raw48+,Total_Pending_Sectors",
+		"198,offlinescanuncsectorct" => "198,raw48,Offline_Scan_UNC_SectCt", // TODO? there goes some `get_unc_attr_id` reference
+		"198,increasing" => "198,raw48+,Total_Offl_Uncorrectabl",
+		"200,writeerrorcount" => "200,raw48,Write_Error_Count",
+		"201,detectedtacount" => "201,raw48,Detected_TA_Count",
+		"220,temp" => "220,tempminmax,Temperature_Celsius",
+		s => s,
+	};
 	// FIXME strings to bytes to strings again… sounds really stupid
 	match parse_standard(s.as_bytes()) {
 		nom::IResult::Done(_, attr) => Ok(attr),
