15a94cb9c151ff606d5399ada2a53a1ce66d268b
vthriller <unixway.drive+git@gmail.com>
2017-10-30 15:07:12 +0300

    sense: avoid barely usable structs

--- src/data/sense/mod.rs
+++ src/data/sense/mod.rs
@@ -2,29 +2,24 @@ mod fixed;
 pub use self::fixed::FixedData;
 
 #[derive(Debug)]
-pub enum SenseData<'a> {
+pub enum Sense<'a> {
 	Fixed(FixedData<'a>),
 	Descriptor, // TODO
-}
-
-#[derive(Debug)]
-pub struct Sense<'a> {
-	pub deferred: bool,
-	pub data: SenseData<'a>,
 }
 
 /**
 Parses sense data of any of the supported formats (70h–73h).
 
-Returns `None` if:
+Returns tuple `(deferred, data)`, where `deferred` indicates whether this sense represents current or deferred error; or `None` if:
 
 * format is not recognized,
 * `data` buffer has not enough data to decode sense.
 
 ## Panics
+
 Panics if `data` is empty.
 */
-pub fn parse(data: &[u8]) -> Option<Sense> {
+pub fn parse(data: &[u8]) -> Option<(bool, Sense)> {
 	let response_code = data[0] & 0x7f;
 	let (fixed, deferred) = match response_code {
 		0x70 => (true, false),
@@ -35,13 +30,10 @@ pub fn parse(data: &[u8]) -> Option<Sense> {
 	};
 
 	let data = if fixed {
-		fixed::parse(data).map(|data| SenseData::Fixed(data))
+		fixed::parse(data).map(|data| Sense::Fixed(data))
 	} else {
-		Some(SenseData::Descriptor)
+		Some(Sense::Descriptor)
 	};
 
-	data.map(|data| Sense {
-		deferred: deferred,
-		data: data,
-	})
+	data.map(|data| (deferred, data))
 }
