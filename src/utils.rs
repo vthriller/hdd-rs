8a9e656c57f41629a1382c2f3e6d5f0e274b731e
vthriller <unixway.drive+git@gmail.com>
2017-12-19 15:00:38 +0300

    utils: fix latest lines for hex dumps of buffers that are not multiple of 16 (or 8) long

--- src/utils.rs
+++ src/utils.rs
@@ -31,12 +31,13 @@ fn pretty_char_from_u8(x: u8) -> char {
 
 #[cfg_attr(feature = "cargo-clippy", allow(needless_range_loop))]
 pub fn hexdump_8(data: &[u8]) -> String {
-	// 3× len for ' {:02x}'
+	// 3× (len rounded up to the multiple of 16) for ' {:02x}'
 	// len/16 for \n
 	// len/16 for ' ' before ascii
-	// len for ascii
+	// len (really rounded up len, because I'm lazy) for ascii
 	// 2 to "round" (/16)s up and have lesser chance of reallocation
-	let mut dump = String::with_capacity(4*data.len() + data.len()/8 + 2);
+	let l = data.len();
+	let mut dump = String::with_capacity(4*(l + 16 - l % 16) + l/8 + 2);
 	let mut ascii = String::with_capacity(16);
 
 	for i in 0..data.len() {
@@ -49,6 +50,12 @@ pub fn hexdump_8(data: &[u8]) -> String {
 		dump.push_str(&format!(" {:02x}", data[i]));
 		ascii.push(pretty_char_from_u8(data[i]));
 	}
+	// align trailing ascii
+	if data.len() % 16 != 0 {
+		for _ in 0..16 - data.len() % 16 {
+			dump.push_str("   ");
+		}
+	}
 	dump.push(' ');
 	dump.push_str(&ascii);
 	ascii.truncate(0);
@@ -58,12 +65,13 @@ pub fn hexdump_8(data: &[u8]) -> String {
 
 #[cfg_attr(feature = "cargo-clippy", allow(needless_range_loop))]
 pub fn hexdump_16be(data: &[u16]) -> String {
-	// 5× len for ' {:04x}'
+	// 5× (len rounded up to the multiple of 8) for ' {:04x}'
 	// len/8 for \n
 	// len/8 for ' ' before ascii
-	// 2× len for ascii
+	// 2× len (really rounded up len, because I'm lazy) for ascii
 	// 2 to "round" (/8)s up and have lesser chance of reallocation
-	let mut dump = String::with_capacity(7*data.len() + data.len()/4 + 2);
+	let l = data.len();
+	let mut dump = String::with_capacity(7*(l + 8 - l%8) + data.len()/4 + 2);
 	let mut ascii = String::with_capacity(16);
 
 	for i in 0..data.len() {
@@ -76,6 +84,12 @@ pub fn hexdump_16be(data: &[u16]) -> String {
 		dump.push_str(&format!(" {:04x}", data[i]));
 		ascii.push(pretty_char_from_u8((data[i] >> 8) as u8));
 		ascii.push(pretty_char_from_u8((data[i] & 0xff) as u8));
+	}
+	// align trailing ascii
+	if data.len() % 8 != 0 {
+		for _ in 0..8 - data.len() % 8 {
+			dump.push_str("     ");
+		}
 	}
 	dump.push(' ');
 	dump.push_str(&ascii);
