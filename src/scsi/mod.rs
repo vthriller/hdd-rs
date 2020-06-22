0e7db13bf10fbabb3d63967380f1ece67f6f1bb4
vthriller <unixway.drive+git@gmail.com>
2020-06-23 01:49:48 +0300

    accept (Ok, 0, 0) as a valid sense code combination for ATA PASS-THROUGH (16)
    This change makes it possible to monitor ATA devices in at least the following configuration:
    
    $ udevadm info -q all -n /dev/sda | fgrep DEVPATH
    E: DEVPATH=/devices/pci0000:00/0000:00:1f.2/host0/target0:0:0/0:0:0:0/block/sda
    
    $ lspci -nn | fgrep 1f.2
    00:1f.2 SATA controller [0106]: Intel Corporation C600/X79 series chipset 6-Port SATA AHCI Controller [8086:1d02] (rev 06)
    
    $ uname -rms
    Linux 3.2.0-100-generic x86_64

--- src/scsi/mod.rs
+++ src/scsi/mod.rs
@@ -322,6 +322,16 @@ pub trait SCSICommon: Sized {
 				descriptors
 			},
 
+			sense::Sense::Descriptor(sense::DescriptorData {
+				descriptors,
+				// some devices/drivers return (Ok, 0, 0) as a sense;
+				// will validate its contents below
+				key: 0x00, asc: 0x00, ascq: 0x00,
+				..
+			}) => {
+				descriptors
+			},
+
 			sense::Sense::Fixed(sense::FixedData::Valid {
 				// Illegal Request / INVALID COMMAND OPERATION CODE
 				key: 0x05, asc: 0x20, ascq: 0x00, ..
